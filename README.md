f8cd618d56f1ef9dddd63a157018ba03819e9880
WintonChurchill <georgevaughan135@gmail.com>
2025-09-02 20:00:21 +1200

    Seperated CSS from HTML, fixed bugs, menu overhaul
    Seperated CSS from HTML, Fixed google maps, Simplified elements, Begun fixing alignment errors between resolutions on a couple of the pages. Completely overhauling the Menu function to be infinitely scaling with database, added .gitignore, Requirements.txt.

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# 204B_Ted-s_Cafe
+# 205B_Ted-s_Cafe
 205 Group Project - Cafe Website
---
+++ Teds_cafe.py
@@ -0,0 +1,37 @@
+from flask import Flask, render_template, redirect, url_for, request, flash, session
+import os 
+
+app = Flask(__name__)
+
+items = [1,2,3,4,5,6,7,8]
+poo = {
+    'item1': 4.00,
+    'item2': 3.00
+}
+@app.route('/')
+def home(): 
+    return render_template('menu.html')
+
+@app.route('/contact_us')
+def contact(): 
+    return render_template('contact_us.html')
+
+@app.route('/menu')
+def menu(): 
+    return render_template('menu.html', item=poo) #"""DB_items""" parse here
+
+@app.route('/checkout_pay')
+def checkout(): 
+    return render_template('checkout_pay.html')
+
+@app.route('/checkout')
+def checkout_1(): 
+    return render_template('checkout.html')
+
+@app.route('/about_us')
+def about_us(): 
+    return render_template('about_us.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
