765bf6c61bae5e193bc221529d23b788b8ecbcff
WintonChurchill <georgevaughan135@gmail.com>
2025-09-08 17:01:12 +1200

    Beginning of sessions migration
    Migrating so it persists across uses

--- Teds_cafe.py
+++ Teds_cafe.py
@@ -3,14 +3,16 @@ import os
 
 app = Flask(__name__)
 
-items = [1,2,3,4,5,6,7,8]
+app.secret_key = os.urandom(24)  # Secret key for session management
+
+Itemlist = []
 
 
 """DEBUG!!! This is a list of dictionaries, each dictionary represents an item with its name as the key and price as the value.-------------------------------------------------
 This data will come from a database. All of the items must be strings, in the format. Current max reccomended name length is 84 at a stretch. """,
 DB_ITEMS = [
     # first item has silly name to test formatting with long strings. DEBUG
-    {'id': '1', 'image': 'https://i.postimg.cc/FFYNBdR5/flat-white.jpg', 'name': 'Flat White Of your dreams PHD Esq the fifth prince of edenburogh the butcher of cork', 'price': '5.00'},
+    {'id': '1', 'image': 'https://i.postimg.cc/FFYNBdR5/flat-white.jpg', 'name': 'Flat White', 'price': '5.00'},
     {'id': '2', 'image': 'https://i.postimg.cc/4NtZXDVW/latte.jpg', 'name': 'Latte', 'price': '5.50'},
     {'id': '3', 'image': 'https://i.postimg.cc/mgs4kmSB/hot-chocolate.jpg', 'name': 'Hot Chocolate', 'price': '5.00'},
     {'id': '4', 'image': 'https://i.postimg.cc/YCzHWQXg/black-coffee.jpg', 'name': 'Black Coffee', 'price': '4.50'},
@@ -28,22 +30,70 @@ def home():
 def contact(): 
     return render_template('contact_us.html')
 
-@app.route('/menu')
-def menu(): 
-    return render_template('menu.html', items=DB_ITEMS) #"""DB_items""" parse here
-
-@app.route('/checkout_pay')
-def checkout(): 
-    return render_template('checkout_pay.html')
-
-@app.route('/checkout')
-def checkout_1(): 
-    return render_template('checkout.html')
-
 @app.route('/about_us')
 def about_us(): 
     return render_template('about_us.html')
 
+@app.route('/menu')
+def menu(): 
+    return render_template('menu.html', items=DB_ITEMS) #"""DB_items""" parse here
+
+@app.route('/add_to_cart', methods=['POST'])
+def add_to_cart():
+    item_id = request.form.get('item_id')
+    qty = int(request.form.get('qty', 1))
+
+    item = next((i for i in DB_ITEMS if i['id'] == item_id), None)
+    if item:
+        cart = session.get('cart', [])
+
+        # Check if item already in cart
+        existing_item = next((i for i in cart if i['id'] == item_id), None)
+        if existing_item:
+            existing_item['qty'] += qty
+        else:
+            cart.append({
+                'id': item['id'],
+                'name': item['name'],
+                'price': float(item['price']),
+                'qty': qty,
+                'image': item['image']
+            })
+
+        session['cart'] = cart  # Save back to session
+
+    return redirect(url_for('menu'))
+
+
+@app.route('/checkout')
+def checkout():
+    cart = session.get('cart', [])
+    total = sum(i['price'] * i['qty'] for i in cart)
+    return render_template('checkout.html', cart=cart, total=total)
+
+@app.route('/checkout_pay')
+def checkout_pay(): 
+    return render_template('checkout_pay.html')
+
+@app.route('/remove_from_cart', methods=['POST'])
+def remove_from_cart():
+    item_id = request.form.get('item_id')
+    cart = session.get('cart', [])
+    cart = [i for i in cart if i['id'] != item_id]
+    session['cart'] = cart
+    return redirect(url_for('checkout'))
+
+@app.route('/update_cart', methods=['POST'])
+def update_cart():
+    item_id = request.form.get('item_id')
+    qty = int(request.form.get('qty', 1))
+    cart = session.get('cart', [])
+    for item in cart:
+        if item['id'] == item_id:
+            item['qty'] = qty
+            break
+    session['cart'] = cart
+    return redirect(url_for('checkout'))
 
 if __name__ == '__main__':
     app.run(debug=True)
